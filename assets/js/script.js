153fe49d91f82c5040aa2ca1b7c3d8a256085f58
Adrian <h7qju7ts@students.codeinstitute.net>
2025-08-03 07:39:29 +0100

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScrore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMutiplyQuestion() {
+
+}
